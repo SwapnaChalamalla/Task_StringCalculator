ce2d6c3f25892accc3a068bcf7fd493a138ef054
SwapnaChalamalla <46569886+SwapnaChalamalla@users.noreply.github.com>
2019-01-21 23:04:16 +0530

    First commit

---
+++ AddCalculator/AddCalculator/AddCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class AddCalculator
+    {
+        /// <summary>
+        /// Seperates numbers from string and make sum
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int Add(string numbers)
+        {
+
+            int result = 0;
+            if (!string.IsNullOrEmpty(numbers))
+            {
+                //Seperate numbers from string 
+                var lstNumbers = GetNumbersfromString(numbers);             
+
+                result = lstNumbers.Sum();
+            }
+            return result;
+        }
+
+     
+        /// <summary>
+        /// Seperates the numbers from string and converts to list
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static List<int> GetNumbersfromString(string numbers)
+        {
+            int z = 0;
+            string strNumbers = numbers.Replace("\\n", ",");
+            char[] delimiterChars = { ' ', ',', ';', '\n', '@', '#' };
+            List<int> lstNumber = strNumbers.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries).Where(x => int.TryParse(x, out z))
+                .Select(x => z).ToList();
+
+            return lstNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the input");
+            string numbers = Console.ReadLine();
+
+            //Create object and invoke the add method
+            int result = new AddCalculator().Add(numbers);
+
+            Console.WriteLine("The result is " + " " + result);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ AddCalculator/AddCalculatorTest/AddCalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddCalculatorTest
+{
+    [TestFixture]
+    public class AddCalculatorTest
+    {
+        AddCalculator objAddCalculator = new AddCalculator();
+
+        [Test]
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("4", 4)]
+        public void Test_EmptyStringAndSingleDigit(string numbers, int expected)
+        {
+            //Act
+            int result = objAddCalculator.Add(numbers);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));   
+        }
+
+       
+    }
+}
